ec7320c006837df4ac650701e59df3d77c6ca5b2
Akio <akio.orihara@outlook.com>
2020-06-29 15:11:07 -0500

    fuction for open_connection

--- jobs/app.py
+++ jobs/app.py
@@ -3,9 +3,14 @@ import sqlite3
 
 PATH = 'db/jobs.sqlite'
 
-def open_connection()
- getattr()
+def open_connection:
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PAHT)
+    connection.row_factory = sqlite3.Row
+    return connection
 
+    
 app = Flask(__name__)
 
 @app.route('/')
