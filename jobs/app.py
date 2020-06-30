3843d6ab8a966e0be5d53685bfc46aff9cb26b54
akioorihara <56176776+akioorihara@users.noreply.github.com>
2020-06-29 19:59:15 -0500

    corrected some typos

--- jobs/app.py
+++ jobs/app.py
@@ -6,7 +6,7 @@ PATH = 'db/jobs.sqlite'
 def open_connection:
     connection = getattr(g, '_connection', None)
     if connection == None:
-        connection = g._connection = sqlite3.connect(PAHT)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
