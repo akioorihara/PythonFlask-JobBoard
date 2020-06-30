63c3375a3ab62cf09032283132ae6bb36255641c
akioorihara <56176776+akioorihara@users.noreply.github.com>
2020-06-30 08:08:54 -0500

    getting error
    trying to midigate this issue for app

--- jobs/app.py
+++ jobs/app.py
@@ -20,7 +20,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     curser.close()
     return results
 
-@app.teardown_appcontext
+#@app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
